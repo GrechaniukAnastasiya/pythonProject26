09046aff4c00e887d8a2d69c42ce034fa61c7b18
RomashkaLyaLya <116283354+RomashkaLyaLya@users.noreply.github.com>
2023-05-02 18:54:45 +0300

    другий приклад обгортки функції

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #Теорія Декоратори
 #1
+
 def my_decorator_func(func): #3
     def wrapper():
         print("Love you")
@@ -11,3 +12,17 @@ def my_decorator_func(func): #3
 def say_hello(): #1
     print("Hello!")
 say_hello()
+
+#2
+import time
+
+def delay_decoraton(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(3)
+        return func(*args, **kwargs)
+    return wrapper
+
+@delay_decoraton
+def sleepy():
+    print('я сплю')
+sleepy()
