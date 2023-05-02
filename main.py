c959b4275907b6d2083bf8797b86b21117f81806
RomashkaLyaLya <116283354+RomashkaLyaLya@users.noreply.github.com>
2023-05-02 19:17:28 +0300

    кешування (фібоначі)

--- main.py
+++ main.py
@@ -26,3 +26,21 @@ def delay_decoraton(func):
 def sleepy():
     print('я сплю')
 sleepy()
+
+#3
+
+def chache_decoraton(func):
+    cache = {}
+    def wrapper(n):
+        if n not in cache:
+            cache[n] = func(n)
+        return cache[n], cache
+    return wrapper
+
+@chache_decoraton
+def fibonacci(n):
+    if n in(0,1):
+        return n
+    return fibonacci(n-1) + fibonacci(n - 2)
+print(fibonacci(10))
+
